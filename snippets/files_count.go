63d38a03ff754070e008133eb3dda846af9d3349
Alex Solovey <naghtigall@gmail.com>
2023-05-25 19:27:40 +0300

    Create files_count.go

---
+++ snippets/files_count.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"path"
+)
+
+type Row struct {
+	ID       int
+	Filename string
+}
+type List struct {
+	FileList []Row
+}
+
+func countGoFiles(folder string) (int, List) {
+	Error := "Error"
+	files, err := os.ReadDir(folder)
+	if err != nil {
+		log.Printf(Error)
+	}
+	Struct1 := List{}
+	Struct2 := Row{}
+	count := 0
+	for _, f := range files {
+		if path.Ext(f.Name()) == ".sql" {
+			//fmt.Println(f.Name())
+			Struct2 = Row{count, f.Name()}
+			Struct1.FileList = append(Struct1.FileList, Struct2)
+			count++
+		}
+	}
+	//fmt.Printf("%+v", Struct1)
+	return count, Struct1
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	folder := "/Volumes/ARCHIVE/PRIVATE/WS/github/lets-go/db_backups"
+	counter, Struct := countGoFiles(folder)
+	log.Println(counter)
+	//fmt.Printf("%+v", Struct)
+	data, err := json.Marshal(Struct)
+	if err != nil {
+		return
+	}
+	w.Write([]byte(data))
+}
+
+func main() {
+
+	//countGoFiles(folder)
+	//fmt.Printf("Count of files: %d\n", count)
+	http.HandleFunc("/", home)
+	log.Println("Starting server on :4200")
+	err := http.ListenAndServe(":4200", nil)
+	log.Fatal(err)
+}
